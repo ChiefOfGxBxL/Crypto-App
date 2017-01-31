52eb546e98bb7d8c5cd9729072a489b572c1fb84
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-01-30 19:26:54 -0500

    Create some interfaces we'll be using, and some sample sensors.

---
+++ App/AccelerometerSensor.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Hardware;
+
+namespace App
+{
+    public class AccelerometerSensor : Sensor, ISensorEventListener
+    {
+        // private _sensorManager
+
+        public AccelerometerSensor()
+        {
+            // TODO: follow the tutorial below to listen to the accelerometer data
+            throw new NotImplementedException();
+        }
+
+        public string getCurrentValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntPtr Handle
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        
+
+        public void OnAccuracyChanged(Android.Hardware.Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ App/GyroscopeSensor.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Hardware;
+
+namespace App
+{
+    class GyroscopeSensor : Sensor, ISensorEventListener
+    {
+        public IntPtr Handle
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getCurrentValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAccuracyChanged(Android.Hardware.Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
