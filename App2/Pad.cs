84b25ddf1187926faf0681bfe6180c710585b4ac
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-02-15 12:59:38 -0500

    Create new classes for Pads and mananging user contacts.

---
+++ App2/Pad.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+
+namespace App2
+{
+    public class Pad
+    {
+        public string Path { get; private set; }
+        public string Username { get; private set; }
+
+        private StreamReader sr;
+
+        public Pad(string path, string username = "")
+        {
+            Path = path;
+            Username = username;
+
+            sr = new StreamReader(path);
+        }
+
+        /// <summary>
+        /// Retrieves the Nth pad in the pad file.
+        /// Note that n should start at 1 for the 1st pad.
+        /// </summary>
+        /// <param name="n">Index of pad to retrieve, starting at n=1 for 1st pad</param>
+        /// <returns></returns>
+        public string GetNthPad(int n)
+        {
+            if (n == 0) return null; // must index starting at 1
+            
+            // eat up the lines until we get to the one we want
+            // TODO: verify there is not an off-by-one error here
+            for(int i = 0; i < n-1; i++)
+            {
+                sr.ReadLine();
+            }
+
+            return sr.ReadLine();
+        }
+
+        public void AppendPads(string[] pads)
+        {
+            // TODO: appends more pads to this current padfile. This is 
+            // not a destructive action -- it does not overwrite the current
+            // pads that are currently in the file.
+        }
+
+        public void DestroyPad()
+        {
+            // TODO: destroys all the pads in this file
+            // this may be for security reasons, initiated by a user request, etc.
+            // it destroys the pad.txt file as well
+        }
+    }
+}
---
+++ App2/PadManager.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App2
+{
+    public class PadManager
+    {
+        private string _appDir;
+        private string _contactsDir;
+
+        public PadManager(string path)
+        {
+            _appDir = path;
+            _contactsDir = Path.Combine(_appDir, "contacts");
+
+            CreateDirIfNotExists(Path.Combine(_appDir, "contacts")); // ensures the contacts path exists
+        }
+
+        /// <summary>
+        /// Creates a directory at the specified path if it does not exist.
+        /// Returns the path.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private string CreateDirIfNotExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            return path;
+        }
+
+        public Pad GetPadForUsername(string username)
+        {
+            if (username.Length == 0) return null; // must specify username
+
+            // The pad is located in the 'contacts' folder, then in a folder named after the user
+            string userContactDir = Path.Combine(_contactsDir, username);
+            string padFilePath = Path.Combine(userContactDir, "pad.txt");
+
+            if(File.Exists(padFilePath))
+            {
+                return new Pad(padFilePath, username);
+            }
+            else
+            {
+                // Here the pad file does not exist for the given contact
+                // We could inform the user that something went wrong, and that they
+                // need to generate more pads for this contact
+                Console.WriteLine("Error: could not find pad.txt file for username " + username);
+                return null;
+            }
+        }
+
+        public Pad CreateNewPadForUsername(string username, string[] pads)
+        {
+            // TODO: creates a new padbook for a username, initializing
+            // the padbook with specified pads
+            return null;
+        }
+    }
+}
