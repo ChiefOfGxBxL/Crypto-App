d504fe8fec8d5694776b3bcf986b71d16c58ea87
ChiefOfGxBxL <chiefofgxbxl@github.com>
2017-03-13 16:20:46 -0400

    Send and receive messages from contact: hits endpoint to interact with message API

--- App2/ViewContactActivity.cs
+++ App2/ViewContactActivity.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Net;
 
 namespace App2
 {
@@ -21,13 +22,18 @@ namespace App2
         EditText sendMessageText;
         Button sendMessageBtn;
 
+        string contactName; // stores which user we are currently viewing
+        WebClient wc; // for accessing API endpoints (retrieving and sending messages)
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.ViewContact);
 
             // Get passed information
-            var contactName = Intent.GetStringExtra("contact");
+            string[] splitName = Intent.GetStringExtra("contact").Split('/');
+            contactName = splitName[splitName.Length - 1];
+            Toast.MakeText(ApplicationContext, contactName, ToastLength.Long).Show();
 
             // Set the global UI variables here
             getMessagesBtn = FindViewById<Button>(Resource.Id.button1);
@@ -36,7 +42,73 @@ namespace App2
             sendMessageText = FindViewById<EditText>(Resource.Id.editText1);
             sendMessageBtn = FindViewById<Button>(Resource.Id.button3);
 
-            this.Window.SetTitle(contactName);
+            getMessagesBtn.Click += GetMessagesBtn_Click;
+            sendMessageBtn.Click += SendMessageBtn_Click;
+
+            // Instantiate the WebClient we will be using to make requests to the API
+            wc = new WebClient();
+        }
+
+        private void SendMessageBtn_Click(object sender, EventArgs e)
+        {
+            bool msgSent = SendMessage(sendMessageText.Text);
+            if(!msgSent)
+            {
+                Toast.MakeText(ApplicationContext, "Error sending message", ToastLength.Long).Show();
+            }
+        }
+
+        private void GetMessagesBtn_Click(object sender, EventArgs e)
+        {
+            messageText.Text = RetrieveMessage();
+        }
+
+        /// <summary>
+        /// Retrieves a message from the server, and if it exists decrypts and displays it
+        /// </summary>
+        private string RetrieveMessage()
+        {
+            // endpoint: GET /messages/:to/:from; in this case, we want to find a message to me, from the user we are viewing
+            string url = "http://safe-inlet-16663.herokuapp.com/messages/" + Identity.Username + "/" + contactName;
+            string msg = wc.DownloadString(url);
+
+            // TODO: decrypt message using corresponding pad
+            var decryptedMsg = msg; // Decrypt here..
+
+            return decryptedMsg;
+        }
+
+        /// <summary>
+        /// Sends the specified (unencrypted) message to the user we are currently viewing
+        /// </summary>
+        /// <param name="msg">Unencrypted string to send</param>
+        /// <returns>Boolean: true if successful, false otherwise</returns>
+        private bool SendMessage(string msg)
+        {
+            if (msg == null || msg.Length == 0) return false; // must have a message to send
+
+            string encryptedMsg = msg; // TODO: encrypt the message here using the pad
+
+            string url = string.Format(
+                "http://safe-inlet-16663.herokuapp.com/messages/new?to={0}&from={1}&content={2}",
+                contactName, // to this contact
+                Identity.Username, // from current user
+                encryptedMsg // message to send along
+            );
+
+            string response = wc.DownloadString(url); // message success will have an empty reponse body
+
+            if (response == null || response == "")
+            {
+                // Success!
+                return true;
+            }
+            else
+            {
+                // Error sending message
+                Toast.MakeText(ApplicationContext, "Error sending message", ToastLength.Long).Show();
+                return false;
+            }
         }
     }
 }
